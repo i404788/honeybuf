845c1c0d9862370ad7eaa95d0e0f2ae2c8155b37
i404788 <404788@student.fontys.nl>
2019-12-28 21:02:39 +0000

    Export typing

--- src/misc/tstools.ts
+++ src/misc/tstools.ts
@@ -1,8 +1,8 @@
-type NonMethodKeys<T> = ({[P in keyof T]: T[P] extends Function ? never : P } & { [x: string]: never })[keyof T];  
-type RemoveMethods<T> = Pick<T, NonMethodKeys<T>>; 
-type RequireOne<T> = (keyof T) extends infer K ?
+export type NonMethodKeys<T> = ({[P in keyof T]: T[P] extends Function ? never : P } & { [x: string]: never })[keyof T];  
+export type RemoveMethods<T> = Pick<T, NonMethodKeys<T>>; 
+export type RequireOne<T> = (keyof T) extends infer K ?
     K extends keyof T ?
     Required<Pick<T, K>> : never : never
 
-type Constructor<T> = {new(...args: any): T, [key: string]: any}
-type StrictConstructor<T, A extends any[]> = {new(...args: A): T, [key: string]: any}
+export type Constructor<T> = {new(...args: any): T, [key: string]: any}
+export type StrictConstructor<T, A extends any[]> = {new(...args: A): T, [key: string]: any}
